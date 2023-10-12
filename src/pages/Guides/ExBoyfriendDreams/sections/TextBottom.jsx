e45cf15a252f11ec797a38ca059a1adba54dd7f2
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-10-12 15:28:13 +0100

    html readability improvements

--- src/pages/Guides/ExBoyfriendDreams/sections/TextBottom.jsx
+++ src/pages/Guides/ExBoyfriendDreams/sections/TextBottom.jsx
@@ -13,27 +13,32 @@ function TextBottom() {
     <MKBox component="section" py={6} mt={2}>
       <Container>
         <Grid container spacing={3} item xs={12} lg={8} mx="auto">
-          <MKTypography variant="body2">
-            <MKTypography variant="h4" mb={2}>
-              Ex boyfriends in modern culture
-            </MKTypography>
-            <br />
-            <MKTypography variant="h5" mb={2}>
-              Taylor Swift
-            </MKTypography>
+          <MKTypography variant="h4" mb={2}>
+            Ex boyfriends in modern culture
+          </MKTypography>
+          <MKTypography variant="body2" mb={6}>
+            Diving into the topic of ex boyfriends within the dreamscapes of modern culture, we
+            share stories from celebrities who have shared their ex-boyfriend dreams.
+          </MKTypography>
+          <MKTypography variant="h5" mb={2}>
+            Taylor Swift
+          </MKTypography>
+          <MKTypography variant="body2" mb={3}>
             In her song &#39;All Too Well&#39; Taylor Swift reflects on a past relationship,
             including dream sequences where she recalls memories with her ex-boyfriend. The dreams
             and the emotions associated with them play a significant role in the song&#39;s
             narrative, demonstrating the impact of dreaming about an ex on processing past
             experiences.
-            <br />
-            <MKTypography variant="h5" mb={2}>
-              Ariana Grande
-            </MKTypography>
+          </MKTypography>
+          <MKTypography variant="h5" mb={2}>
+            Ariana Grande
+          </MKTypography>
+          <MKTypography variant="body2" mb={2}>
             Furthermore, in the song &#39;Thank U, Next&#39; by Ariana Grande, she mentions dreaming
             about her ex-boyfriends, referencing her personal growth and gratitude for the lessons
             learned from those relationships.
-            <br />
+          </MKTypography>
+          <MKTypography variant="body2" mb={2}>
             Dreams about ex-boyfriends can have multiple meanings, depending on the context and the
             individual&#39;s underlying continue feelings. They can symbolize our longing for
             security, our fear of being vulnerable in a new relationship, and our need for closure.
